f6e1bb6c448354079d67c0130bdaa890a29e086b
SamuraiT <4782857+SamuraiT@users.noreply.github.com>
2022-08-11 15:26:37 +0900

    feat: Add transfer test

--- test/Henkaku.ts
+++ test/Henkaku.ts
@@ -1,11 +1,40 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 
 describe("Henakaku Token", () => {
-  it ('', async () => {
-      const [owner, otherAccount] = await ethers.getSigners();
-      const Henkaku = await ethers.getContractFactory("HenkakuToken");
+  let owner: SignerWithAddress,
+    alice: SignerWithAddress,
+    bob: SignerWithAddress,
+    funds: SignerWithAddress,
+    erc20: Contract;
+
+  beforeEach(async () => {
+    [owner, alice, bob, funds] = await ethers.getSigners();
+    const ERC20 = await ethers.getContractFactory("HenkakuToken");
+    erc20 = await ERC20.connect(owner).deploy();
+    await erc20.deployed();
+  })
+
+  describe('transfer', () => {
+    it('sucessfully transfer', async () => {
+      await erc20.addWhitelistUsers([owner.address, alice.address, bob.address])
+      await erc20.connect(owner).mint(alice.address, parseUnits('100', 18))
+
+      await expect(
+        erc20.connect(alice).transfer(bob.address, 20)
+      ).to.changeTokenBalances(erc20, [alice, bob], [-20, 20]);
+    })
+
+    it('reverts if receiver is not allowed', async () => {
+      await erc20.addWhitelistUsers([owner.address, alice.address])
+      await erc20.connect(owner).mint(alice.address, parseUnits('100', 18))
+
+      await expect(
+        erc20.connect(alice).transfer(bob.address, 20)
+      ).to.be.revertedWith('INVALID: RECEIVER IS NOT ALLOWED')
+    })
   })
 })
